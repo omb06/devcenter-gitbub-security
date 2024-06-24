d0f5e592ba7a04720bf0e86f378485555c3befb9
Oumy MBENGUE <94635909+omb06@users.noreply.github.com>
2024-06-24 16:37:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-const app = require("express")(),
-      pg = require("pg"),
-      pool = new pg.Pool(config);
+var app = require('express')();
 
-app.get("search", function handler(req, res) {
-  // BAD: the category might have SQL special characters in it
-  var query1 =
-    "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" +
-    req.params.category +
-    "' ORDER BY PRICE";
-  pool.query(query1, [], function(err, results) {
-    // process results
-  });
+app.get('/user/:id', function(req, res) {
+    let id = req.params.id;
+    id = id.replace(/<|>/g, ""); // BAD
+    let userHtml = `<div data-id="${id}">${getUserName(id) || "Unknown name"}</div>`;
+    // ...
+    res.send(prefix + userHtml + suffix);
 });
